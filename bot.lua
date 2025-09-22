688cb5496f3b4744fde3ef3a8ed7e8e940593c76
SystemNasa <100525301+SystemNasa@users.noreply.github.com>
2025-09-22 15:10:22 +0200

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -102,7 +102,7 @@ local CONFIG = {
     SCRIPT_URL = "https://raw.githubusercontent.com/SystemNasa/roblox/refs/heads/main/bot.lua",
     TOOL_CYCLE_DELAY = 0.05,  -- Very fast tool cycling for lag (NO TTS, NO TELEPORTING)
     TELEPORT_DELAY = 0.1,    -- Slower delay between teleports in annoy mode (was 0.05)
-    TTS_INTERVAL = 30         -- Send TTS every 8 seconds in annoy mode
+    TTS_INTERVAL = 15         -- Send TTS every 8 seconds in annoy mode
 }
 
 local player = define(Players.LocalPlayer)
